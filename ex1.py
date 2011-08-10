e6f6bcd0b452ffd4162c56016c22b02757a4a1e4
silas <silas.villon@corp.globo.com>
2011-08-10 16:24:13 -0300

    mais um

--- ex1.py
+++ ex1.py
@@ -2,20 +2,78 @@
 def oi():
     return "Alo mundo"
 #2
-def informa_numero(num):
+def informaNumero(num):
     a = str(num)
     return "o numero informado foi "+ a
 #3
-#a = int(raw_input('digite um numero: '))
-#b = int(raw_input('digite outro numero:'))
-#print "a soma dos 2 eh ", a+b
+def somaNumeros(a, b):
+    return a+b
 #4
-#lista = raw_input('digite 4 numeros, separe por espacos: ')
-#l = lista.split()
-#n = []
-#i = 0
-#for num in range(4):
-#    n[num] = int(l[num])
-#    i+=n[num]
-#print i
- 
+def media(a, b, c, d):
+    return (a+b+c+d)/4
+#5
+def conversor(metros):
+    return metros*100
+#6
+def circulo(raio):
+    return raio*2*3.14
+#7
+def dobroArea(lado):
+    return (lado**2)*2
+#8
+def calculaSalario(salhora, horas):
+    return salhora*horas
+#9
+def conversorFerenheitCelcius(f):
+    c = 5*(f-32)/9
+    return c
+#10
+def conversorCelciusFarenheit(c):
+    f = (9*c/5)+32
+#11
+def aleatorio1(i1, i2, f):
+    a = (2*i1)*(i2/2)
+    b = (3*i1)+f
+    c = f**3
+    print a
+    print b
+    print c
+#12
+def calculaPesoIdeal(altura):
+    peso = 71.7*altura - 58
+    return peso
+#13
+def calculaPoneisMalditos(altura, sexo, peso):
+    if sexo == 'homem':
+        peso_ideal = (72.7*altura) - 58
+    else: 
+	peso_ideal = (62.1*altura) - 44.7
+    if peso == peso_ideal print "vc eh uma caveira."
+    else if peso < peso_ideal print "vc tah sussa"
+    else print "vc eh um gordo maldito"
+#14
+def pescador(peso):
+    excesso = 0
+    multa = 0
+    if peso > 50 :
+	excesso = peso - 50
+	multa = excesso * 40
+#15
+def aleatorio2(hora, horasmes):
+    bruto = hora*horasmes
+    print "+ Salario Bruto : R$ ", bruto
+    print "- IR (11%) : R$ ", bruto*0.11
+    print "- INSS (8%) : R$ ", bruto*0.08
+    print "- Sindicato ( 5%) : R$ ", bruto*0.05
+    print "= Salario Liquido : R$ ", bruto - bruto*0.24
+#16
+def tintas(area):
+    litros = area/3
+    latas = 0
+	if litros < 18:
+		latas = 1
+	else:
+			
+#17
+#18
+
--- extests.py
+++ extests.py
@@ -1,13 +1,39 @@
 import unittest
 from ex1 import *    
 class TestMain(unittest.TestCase):
-    #def test_coisas(self):
+    #1
     def test_diz_oi(self):
         self.assertEquals("Alo mundo", oi())
-
+    #2
     def test_numero_confere(self):
         self.assertEquals("o numero informado foi 3", informa_numero(3))
+    #3
+    def test_soma_numeros(self):
+        self.assertEquals(5, soma_numeros(2, 3))
+    #4 
+    def test_tira_media(self):
+	self.assertEquals(5, media(5, 5, 5, 5))
+    #5
+    def test_converte_metros_em_centimetros(self):
+	self.assertEquals(500, conversor(5))
+    #6
+    def test_calcula_area_do_circulo(self):
+	self.assertEquals(12.56, circulo(2))
+    #7
+    def test_retorna_dobro_da_area(self):
+		self.assertEquals(8, dobroarea(2))
 
+    #8 pulei			
+    #9
+    #10
+    #11
+    #12
+    #13
+    #14
+    #15
+    #16
+    #17
+    #18
 
 if __name__ == '__main__' :
     unittest.main()
